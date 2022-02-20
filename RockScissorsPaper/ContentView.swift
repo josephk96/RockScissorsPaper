316f78e137ef23ab3570276615b012435bb40bce
Soo Hwan Kim <soohwan@shuttledelivery.co.kr>
2022-02-21 00:56:32 +0900

    Initial commit

--- RockScissorsPaper/ContentView.swift
+++ RockScissorsPaper/ContentView.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+let arrayOfChoices = ["Rock", "Scissors", "Paper"]
+
 struct ContentView: View {
+    @State private var currentChoice = arrayOfChoices.randomElement()
+    @State private var showStartButton = true
+    @State private var shouldPlayerWin = false
+    @State private var showAlert = false
+    @State private var userAnswer = ""
+    
+    func setShouldPlayerWin(_ value: Bool) {
+        shouldPlayerWin = value
+    }
+    
+    func setUserAnswer(_ value: String) {
+        userAnswer = value
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if(showStartButton) {
+            Button("Start Game") {
+                showAlert = true
+                showStartButton = false
+            }
+        } else {
+            Text(currentChoice ?? "")
+                .padding()
+                .alert("Win or Lose?", isPresented: $showAlert) {
+                    Button("Win") {
+                        setShouldPlayerWin(true)
+        
+                    }
+                    Button("Lose", role: .destructive) {
+                        setShouldPlayerWin(false)
+                    }
+                }
+            
+            HStack {
+                ForEach(arrayOfChoices.indices) { index in
+                    Button(action: {setUserAnswer(arrayOfChoices[index])}) {
+                        Text("\(arrayOfChoices[index])")
+                    }
+                }
+            }
+        }
     }
 }
 
