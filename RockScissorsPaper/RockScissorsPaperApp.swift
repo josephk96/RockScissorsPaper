a4e3e65adf2d7371248f0635e70f6ee59fe2850d
Soo Hwan Kim <soohwan@shuttledelivery.co.kr>
2022-02-20 21:41:42 +0900

    Initial Commit

---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by Soo Hwan Kim on 2022/02/20.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
